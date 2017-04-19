fa2973a5661a13733293cf00aebc293a3d6b73b7
William Wang <william.oliver.wang@gmail.com>
2017-04-18 20:50:14 -0400

    Update weatherforecastbot.py

--- weatherforecastbot.py
+++ weatherforecastbot.py
@@ -83,7 +83,7 @@ def get_forecast_message(location):
     message = '{}, {} - {}\n{}\nHigh: {}°F\nLow: {}°F\nAvg: {}°F\n{}'.format(city, country, formatted_forecast_time, weather, high_temp, low_temp, average_temp, advice)
     return message
 
-#Allows weather bot to check if anyone told it to stop every 30 seconds while updating its forecast using threading
+#Allows weather bot to check if anyone told it a command every 30 seconds while updating its forecast using threading
 class checkThread(threading.Thread):
     def __init__(self, stop):
         threading.Thread.__init__(self)
@@ -94,15 +94,16 @@ class checkThread(threading.Thread):
     def run(self):
         print("Starting " + self.name)
         while True:
-            mentions = api.mentions_timeline(count = 1)
+            mentions = api.mentions_timeline(count = 1) #Grabs most recent mention of the bot from Twitter timeline
             for mention in mentions:
                 mention_text = mention.text
-                mention_text = mention_text.replace(bot_twitter_handle + ' ', '')
+                mention_text = mention_text.replace(bot_twitter_handle + ' ', '') #Cuts out bot's twitter handle from tweet bot is mentioned in
                 self.mention_user = '@' + mention.user.screen_name
             print(self.mention_user + ' ' + mention_text)
-            if mention_text.upper() == 'STOP':
+            if mention_text.upper() == 'STOP': #@bot stop
                 self.stop = True
-            if mention_text.split(':')[0].upper() == 'LOCATION':
+            if mention_text.split(':')[0].upper() == 'LOCATION': #@bot location: _____
+                #Formats new location name to look nice when tweeting it back at user for confirmation of change
                 unformatted_location = mention_text.split(':')[1]
                 if unformatted_location.find(',') != -1:
                     city_name = string.capwords(unformatted_location.split(',')[0])
@@ -141,10 +142,10 @@ tweet(location)
 #Bot will continue tweeting updated forecasts every 5 minutes indefinitely until someone tweets at it to stop
 while True:
     time.sleep(30)
-    if check_mention.stop: #Checks if the stopThread has found someone telling the bot to stop
+    if check_mention.stop: #Checks if any mentions of the robot contains the stop command
         break #If someone told bot to stop it shuts down
     if location != check_mention.location:
-        #Checking if the tweeted location change is a valid city name
+        #Checking if the tweeted location change is a valid city name, if it's not valid location doesn't get changed
         forecast_payload = {'q': check_mention.location, 'cnt': '1', 'appid': apikey, 'type': 'accurate'}
         forecast_request = requests.get('http://api.openweathermap.org/data/2.5/forecast?', params = forecast_payload)
         forecast = json.loads(forecast_request.text)
